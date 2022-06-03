4cb9211ae22bfcc4800139fff2a2f9e97afececa
ankitha <ankitha.codes@gmail.com>
2022-06-03 17:06:21 +0530

    added all type of aspects

--- src/main/java/com/example/aopoverview/controllers/MainController.java
+++ src/main/java/com/example/aopoverview/controllers/MainController.java
@@ -5,12 +5,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class MainController {
 
+    //method to check all aspects except AfterThrowing
     public String getStudentDetail(Integer studentId) {
         // do some processing
         System.out.println("Actual class: returning student details");
         return "studentDetail";
     }
 
+    //method to check aspect - AfterThrowing
     public void getStudentError() {
         System.out.println("Actual class: Error returning student details");
         Integer res = 50/0;
