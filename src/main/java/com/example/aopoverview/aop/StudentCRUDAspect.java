e8b6f913d586a937bca95b30ca9bab146080b992
ankitha <ankitha.codes@gmail.com>
2022-08-05 17:45:45 +0530

    corrected display messages

--- src/main/java/com/example/aopoverview/aop/StudentCRUDAspect.java
+++ src/main/java/com/example/aopoverview/aop/StudentCRUDAspect.java
@@ -12,34 +12,34 @@ public class StudentCRUDAspect {
     @Before("execution(* com.example.aopoverview.controllers.MainController.getStudentDetail(..)) and args(studentId)")     //point-cut expression
     public void logBefore(JoinPoint joinPoint, Integer studentId)
     {
-        System.out.println("Before advice: before MainController.getStudentDetail() : " + joinPoint.getSignature().getName());
+        System.out.println("-Before advice: before MainController.getStudentDetail() : " + joinPoint.getSignature().getName());
     }
     @After("execution(* com.example.aopoverview.controllers.MainController.getStudentDetail(..)) and args(studentId)")      //point-cut expression
     public void logAfter(JoinPoint joinPoint, Integer studentId)
     {
-        System.out.println("After advice: After MainController.getStudentDetail() : " + joinPoint.getSignature().getName());
+        System.out.println("-After advice: After MainController.getStudentDetail() : " + joinPoint.getSignature().getName());
     }
 
     @Around("execution(* com.example.aopoverview.controllers.MainController.getStudentDetail(..)) and args(studentId)")      //point-cut expression
     public void logAround(ProceedingJoinPoint joinPoint, Integer studentId) throws Throwable {
 //        System.out.println("Aspect class: Around MainController.getStudentDetail() : " + joinPoint.getSignature().getName());
-        System.out.println("around advice: Write code for before around advise");
+        System.out.println("-Around advice: Write code for before around advise");
 
         joinPoint.proceed(); //continue to call method (MainController.getStudentDetail)
 
-        System.out.println("around advice: Write code for after around advise");
+        System.out.println("-Around advice: Write code for after around advise");
     }
 
     @AfterReturning("execution(* com.example.aopoverview.controllers.MainController.getStudentDetail(..)) and args(studentId)")      //point-cut expression
     public void logAfterReturning(JoinPoint joinPoint, Integer studentId)
     {
-        System.out.println("afterReturning advice: AfterReturning MainController.getStudentDetail() : " + joinPoint.getSignature().getName());
+        System.out.println("-AfterReturning advice: AfterReturning MainController.getStudentDetail() : " + joinPoint.getSignature().getName());
     }
 
     @AfterThrowing(value = "execution(* com.example.aopoverview.controllers.MainController.getStudentError(..))", throwing = "ex")      //point-cut expression
     public void logAfterThrowing(JoinPoint joinPoint, Exception ex)
     {
-            System.out.println("afterThrowing advice: AfterThrowing MainController.getStudentError() : " + joinPoint.getSignature().getName());
+            System.out.println("-AfterThrowing advice: AfterThrowing MainController.getStudentError() : " + joinPoint.getSignature().getName());
             System.out.println("Exception is:" + ex.getMessage());
     }
 }
--- src/main/java/com/example/aopoverview/controllers/MainController.java
+++ src/main/java/com/example/aopoverview/controllers/MainController.java
@@ -8,13 +8,13 @@ public class MainController {
     //method to check all aspects except AfterThrowing
     public String getStudentDetail(Integer studentId) {
         // do some processing
-        System.out.println("Actual class: returning student details");
+        System.out.println("-Actual class: returning student details");
         return "studentDetail";
     }
 
     //method to check aspect - AfterThrowing
     public void getStudentError() {
-        System.out.println("Actual class: Error returning student details");
+        System.out.println("-Actual class: Error returning student details");
         Integer res = 50/0;
     }
 
